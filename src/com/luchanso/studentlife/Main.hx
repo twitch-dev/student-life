25cd3dc517d3c118bf809a89fe510da6563de681
luchanso <oleg@loutchansky.com>
2016-03-08 13:28:06 +0300

    Initial repository ::stuck_out_tongue_winking_eye:

---
+++ src/com/luchanso/studentlife/Main.hx
@@ -0,0 +1,21 @@
+package com.luchanso.studentlife;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Loutchansky Oleg
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
