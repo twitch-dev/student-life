d2d319ac744af87d7ac5bbd57be47f04fd8fd759
luchanso <oleg@loutchansky.com>
2016-03-11 19:17:47 +0300

    Add ProgressBarLable

---
+++ src/com/luchanso/studentlife/game/ui/statsview/ProgressBarLabel.hx
@@ -0,0 +1,18 @@
+package com.luchanso.studentlife.game.ui.statsview;
+
+import openfl.text.TextField;
+
+/**
+ * ...
+ * @author Loutchansky Oleg
+ */
+class ProgressBarLabel extends TextField
+{
+
+	public function new()
+	{
+		super();
+
+	}
+
+}
