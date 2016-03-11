ec25496faf2d497c48523f146f670040ef8af9f8
luchanso <oleg@loutchansky.com>
2016-03-11 19:16:45 +0300

    Add PogressBar class

---
+++ src/com/luchanso/studentlife/game/ui/statsview/ProgressBar.hx
@@ -0,0 +1,16 @@
+package com.luchanso.studentlife.game.ui.statsview;
+
+import openfl.display.Sprite;
+
+/**
+ * ...
+ * @author Loutchansky Oleg
+ */
+class ProgressBar extends Sprite
+{
+
+	public function new()
+	{
+		super();
+	}
+}
