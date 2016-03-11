681e2f314c11237e12abf8db077059cb97a52b68
luchanso <oleg@loutchansky.com>
2016-03-11 15:29:22 +0300

    Add class emotion

---
+++ src/com/luchanso/studentlife/game/ui/Emotion.hx
@@ -0,0 +1,18 @@
+package com.luchanso.studentlife.game.ui;
+
+import openfl.display.Sprite;
+
+/**
+ * ...
+ * @author Loutchansky Oleg
+ */
+class Emotion extends Sprite
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
