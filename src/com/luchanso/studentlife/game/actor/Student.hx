3b0fa9fc3870b1078b35b79b7ef26ceef0b4cd83
luchanso <oleg@loutchansky.com>
2016-03-11 16:17:33 +0300

    Add propereties in Student class

--- src/com/luchanso/studentlife/game/actor/Student.hx
+++ src/com/luchanso/studentlife/game/actor/Student.hx
@@ -6,26 +6,61 @@ package com.luchanso.studentlife.game.actor;
  */
 class Student
 {
-	private var fed : Float;
-	private var money : Int;
-	private var happy : Float;
-	private var education : Float;
+	private var _fed : Float;
+	private var _money : Float;
+	private var _happy : Float;
+	private var _education : Float;
 
-	public function new() 
+	public var fed(get, set) : Float;
+	public var money(get, set) : Float;
+	public var happy(get, set) : Float;
+	public var education(get, set) : Float;
+
+	public function new()
 	{		
 		fed = 0;
 		money = 0;
 		happy = 0;
 		education = 0;
 	}
-	
-	/**
-	 * Обновление состояния студента
-	 * @param	days
-	 */
-	public function update(days : Float) : Void
+
+	function get_education():Float
 	{
-		
+		return _education;
 	}
-	
+
+	function set_education(value:Float):Float
+	{
+		return _education = value;
+	}
+
+	function get_happy():Float
+	{
+		return _happy;
+	}
+
+	function set_happy(value:Float):Float
+	{
+		return _happy = value;
+	}
+
+	function get_money():Float
+	{
+		return _money;
+	}
+
+	function set_money(value:Float):Float
+	{
+		return _money = value;
+	}
+
+	function get_fed():Float
+	{
+		return _fed;
+	}
+
+	function set_fed(value:Float):Float
+	{
+		return _fed = value;
+	}
 }
