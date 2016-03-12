d51a4a80110180915d3cc44f086e414f52d4be4f
luchanso <oleg@loutchansky.com>
2016-03-12 19:51:22 +0300

    Add ProgressLable Class

--- src/com/luchanso/studentlife/game/ui/statsview/ProgressBarLabel.hx
+++ src/com/luchanso/studentlife/game/ui/statsview/ProgressBarLabel.hx
@@ -1,6 +1,8 @@
 package com.luchanso.studentlife.game.ui.statsview;
 
 import openfl.text.TextField;
+import openfl.text.TextFieldAutoSize;
+import openfl.text.TextFormat;
 
 /**
  * ...
@@ -8,11 +10,16 @@ import openfl.text.TextField;
  */
 class ProgressBarLabel extends TextField
 {
+	private static var fontSize : Int = 14;
 
-	public function new()
+	public function new(text : String = "")
 	{
 		super();
 
+		this.defaultTextFormat = new TextFormat(Config.font, fontSize, Config.colorMainUi);
+		this.text = text;
+		this.selectable = false;
+		this.mouseEnabled = false;
+		this.autoSize = TextFieldAutoSize.LEFT;
 	}
-
 }
