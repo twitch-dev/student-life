7072a89ffff699e5d7ea4d3869d21697bb9942c0
luchanso <oleg@loutchansky.com>
2016-03-14 19:07:05 +0300

    Remove keybardEvent from StatsView

--- src/com/luchanso/studentlife/game/ui/statsview/StatsView.hx
+++ src/com/luchanso/studentlife/game/ui/statsview/StatsView.hx
@@ -29,19 +29,8 @@ class StatsView extends Sprite
 		var tempX = Lib.application.window.width - margin;
 		var tempY = margin;
 
-		Lib.current.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
-
 		addMoneyLable(tempX, tempY);
 		addProgressBars(tempX, tempY + paddingTop);
-	}
-
-	private function keyDown(e:KeyboardEvent):Void
-	{
-		if (e.keyCode == Keyboard.SPACE)
-		{
-			Actuate.tween(progressHappy, 3, { progress: Math.random() } );
-			Actuate.tween(progressFed, 3, { progress: Math.random() } );
-		}
 	}
 
 	private function addMoneyLable(x : Float, y : Float) : Void
@@ -53,10 +42,8 @@ class StatsView extends Sprite
 	private function addProgressBars(x : Float, y : Float) : Void
 	{
 		progressHappy = new ProgressBar(x, y, "Счастье");
-		Actuate.tween(progressHappy, 3, { progress: Math.random() } );
 
 		progressFed = new ProgressBar(x, y + paddingTop, "Сытность");
-		Actuate.tween(progressFed, 3, { progress: Math.random() } );
 
 		addChild(progressHappy);
 		addChild(progressFed);
